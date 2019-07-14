d8ad1c2d0fdded8d3189f760e8eff868a2a1800d
Badar <badar@tentwenty.me>
2019-07-14 09:55:07 +0400

    api key removed

--- src/components/api/moviedbapi.js
+++ src/components/api/moviedbapi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = 'd547ee30f07b5fa5f6c5a2f0f099e635'
+const apiKey = 'API_KEY'
 export default axios.create({
   baseURL: `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`,
  
--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Movie = (props) => {
+class Movie extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imgheight: 0
+        }
+        this.imgRef = React.createRef();
+    }
+    componentDidMount() {
+        this.imgRef.current.addEventListener('load', this.loadedRed);
+    }
+    loadedRed = () => {
+       const imgheight = this.imgRef.current.clientHeight
+       this.setState({
+        imgheight
+       })
+    }
+render() {
+    const {movie} = this.props;
+    const {image} = this.props;
+    
     return(
         <div className="col-sm-3">
             <div className="card">
             <div className="card-image">
-            <Link to={`/movie/${props.movie.id}`}>
-            <img src={`https://image.tmdb.org/t/p/w500${props.image}`} className="img-thumbnail" alt="" />
+            <Link to={`/movie/${movie.id}`}>
+            <img style={{}}ref={this.imgRef} src={`https://image.tmdb.org/t/p/w500${image}`} className="img-thumbnail" alt="" />
             </Link>
             </div>
            
         </div>
         </div>
     )
+}
+   
 }
 
 export default Movie;
